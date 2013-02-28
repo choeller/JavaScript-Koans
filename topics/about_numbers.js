3a650ff2e0ff07dbfa7b048a4715cb9720d8ca1f
OpenShift guest <christoph.hoeller@googlemail.com>
2013-02-28 04:37:52 -0500

    completed unit tests

--- topics/about_numbers.js
+++ topics/about_numbers.js
@@ -2,15 +2,15 @@
 module("About Numbers (topics/about_numbers.js)");
 
 test("types", function() {
-    var typeOfIntegers = typeof(6);
+    //var typeOfIntegers = typeof(6);
     var typeOfFloats = typeof(3.14159);
-    equals(typeOfIntegers === typeOfFloats, __, 'are ints and floats the same type?');
-    equals(typeOfIntegers, __, 'what is the javascript numeric type?');
-    equals(1.0, __, 'what is a integer number equivalent to 1.0?');
+    equals(typeOfIntegers === typeOfFloats, false, 'are ints and floats the same type?');
+    equals(typeOfIntegers, "int, 'what is the javascript numeric type?');
+    equals(1.0, 1, 'what is a integer number equivalent to 1.0?');
 });
 
 test("NaN", function() {
     var resultOfFailedOperations = 7/'apple';
-    equals(isNaN(resultOfFailedOperations), __, 'what will satisfy the equals assertion?');
-    equals(resultOfFailedOperations == NaN, __, 'is NaN == NaN?');
+    equals(isNaN(resultOfFailedOperations), true, 'what will satisfy the equals assertion?');
+    equals(resultOfFailedOperations == NaN, true, 'is NaN == NaN?');
 });
